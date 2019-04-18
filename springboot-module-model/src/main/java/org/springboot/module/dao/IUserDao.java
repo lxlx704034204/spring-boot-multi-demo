1a803997e687b889a8151239cafa84dd7c82cbe2
Administrator <Administrator@cchPc>
2019-04-19 07:40:56 +0800

    repair name mapping

--- springboot-module-model/src/main/java/org/springboot/module/dao/IUserDao.java
+++ springboot-module-model/src/main/java/org/springboot/module/dao/IUserDao.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 @Mapper
 @Repository
 public interface IUserDao {
-	@Select("select * from CCH.T_USER where USER_ID = #{id}")
+	/*@Select("select * from CCH.T_USER where USER_ID = #{id}")*/
+	@Select("select USER_ID as userId, USER_NAME as userName, birthday from CCH.T_USER where USER_ID = #{id}")
 	public User selectById(String id);
 }
