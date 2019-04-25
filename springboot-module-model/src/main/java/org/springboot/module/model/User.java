42dc1b18a01d2d0aa694efdc5c11deba9053d4c7
Administrator <Administrator@cchPc>
2019-04-25 22:43:22 +0800

    add more swagger2 demo on user

--- springboot-module-model/src/main/java/org/springboot/module/model/User.java
+++ springboot-module-model/src/main/java/org/springboot/module/model/User.java
@@ -2,12 +2,26 @@ package org.springboot.module.model;
 
 import java.util.Date;
 
+//import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+//@ApiModel
 public class User {
+	@ApiModelProperty(allowableValues="001,002,003", required=true,value="a description for this property ",example="002")
 	private String userId;
+	
+	@ApiModelProperty(allowableValues="xxx,yyy,zzz", example="zzz", allowEmptyValue=true)
 	private String userName;
+	
+	@ApiModelProperty(hidden=true)
 	private String password;
+	
+	@ApiModelProperty(example="18812345678")
 	private String phone;
+	
+	@ApiModelProperty(value="this is a address property",example="fujian")
 	private String address;
+	
+	@ApiModelProperty(dataType="String")
 	private Date birthday;
 
 	public String getUserId() {
